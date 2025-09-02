4ec43e1edbf79c6b827fb11d1d2412bef6884848
Adarsh Ranjan <adarsh0ranjan@gmail.com>
2025-09-02 18:02:30 +0530

    main activity updated

--- app/src/main/java/com/babelfish/poc/tourist/MainActivity.kt
+++ app/src/main/java/com/babelfish/poc/tourist/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.babelfish.poc.tourist
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
